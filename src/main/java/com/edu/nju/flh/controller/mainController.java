9e89880de738a1722d54e85d41a8adfe10d09da5
fulinhua <flhnju@163.com>
2018-03-24 00:41:22 +0800

    加入了初始化的配置

---
+++ src/main/java/com/edu/nju/flh/controller/mainController.java
@@ -0,0 +1,18 @@
+package com.edu.nju.flh.controller;
+
+/**
+ * Created by fulinhua on 2018/3/24.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+    @Controller
+    public class mainController {
+
+        @RequestMapping("/")
+        String home() {
+            return "main";
+        }
+
+
+    }
