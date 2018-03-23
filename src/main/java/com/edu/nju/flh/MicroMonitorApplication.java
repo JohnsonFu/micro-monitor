d85fcf7c33502f1a5a7e9bfe195276d045a81c6c
fulinhua <flhnju@163.com>
2018-03-24 00:12:52 +0800

    init

---
+++ src/main/java/com/edu/nju/flh/MicroMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.edu.nju.flh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMonitorApplication.class, args);
+	}
+}
