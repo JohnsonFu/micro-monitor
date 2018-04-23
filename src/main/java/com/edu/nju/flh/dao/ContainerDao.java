1528b4159c8bc65475d3d89fdc5d26b87f8d4850
fulinhua <flhnju@163.com>
2018-04-23 19:47:06 +0800

    添加了bootstrap

---
+++ src/main/java/com/edu/nju/flh/dao/ContainerDao.java
@@ -0,0 +1,9 @@
+package com.edu.nju.flh.dao;
+
+/**
+ * Created by fulinhua on 2018/4/22.
+ */
+public interface ContainerDao {
+
+    
+}
---
+++ src/main/java/com/edu/nju/flh/dao/impl/ContainerDaoImpl.java
@@ -0,0 +1,11 @@
+package com.edu.nju.flh.dao.impl;
+
+import com.edu.nju.flh.dao.ContainerDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by fulinhua on 2018/4/22.
+ */
+@Repository
+public class ContainerDaoImpl implements ContainerDao {
+}
